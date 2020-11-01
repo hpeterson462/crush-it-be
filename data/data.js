5528044252e20e3abf2710a8039b18fc5966536b
Heather Peterson <hpeterson462@gmail.com>
2020-10-31 17:02:11 -0700

    data built

---
+++ data/data.js
@@ -0,0 +1,21 @@
+module.exports = [
+  {
+    location: 'Rattlesnake',
+    name: 'Arabesque',
+    rating: '5.10a',
+    notes: 'Scary split legged crux!'
+  },
+  {
+    location: 'Smith Rock',
+    name: 'Purple Headed Warrior',
+    rating: '5.7',
+    notes: 'Nice warm up.'
+  },
+  {
+    location: 'Emigrant Lake',
+    name: 'Aqua Man',
+    rating: '5.10b',
+    notes: 'Better than the Poison Oak Wall.'
+  }
+];
+
